416e4055ecabec64299ca33ccee36e0c6a3ac64d
stevenkurniawan2003 <79078698+stevenkurniawan2003@users.noreply.github.com>
2025-05-05 22:33:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,12 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-// Halaman login
 Route::get('/login', [PageController::class, 'login'])->name('login');
 
-// Proses login (POST)
 Route::post('/login', [PageController::class, 'authenticate']);
 
-// Halaman dashboard (diproteksi)
 Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
 
-// Proses logout
 Route::post('/logout', [PageController::class, 'logout'])->name('logout');
 
 Route::get('/pengelolaan', [PageController::class, 'pengelolaan'])->name('pengelolaan');
